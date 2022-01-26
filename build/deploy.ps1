85a90c1420701b949c397a59650364eaf8859b0c
paule96 <paul-jeschke@outlook.com>
2022-01-26 18:12:12 +0000

    fix output connection for PR comment

--- build/deploy.ps1
+++ build/deploy.ps1
@@ -193,6 +193,6 @@ while (
 Remove-Item -Recurse -Force $workingDir;
 $response.response.siteUrl;
 # echo "::set-output name=pr-title::$(echo $title)"
-"::set-output name=siteurl::$($response.response.siteUrl)";
-echo "::set-output name=siteurl::$($response.response.siteUrl)";
+# "::set-output name=siteurl::$($response.response.siteUrl)";
+# echo "::set-output name=siteurl::$($response.response.siteUrl)";
 Write-Output "::set-output name=SiteUrl::$($response.response.siteUrl)";
