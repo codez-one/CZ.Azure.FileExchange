d6558af9c284db4757231bf9c0327c8cd4f4b766
paule96 <paul-jeschke@outlook.com>
2022-01-26 17:54:37 +0000

    try lower case stuff

--- build/deploy.ps1
+++ build/deploy.ps1
@@ -192,5 +192,6 @@ while (
 
 Remove-Item -Recurse -Force $workingDir;
 $response.response.siteUrl;
-"::set-output name=SiteUrl::$($response.response.siteUrl)";
+# echo "::set-output name=pr-title::$(echo $title)"
+"::set-output name=siteurl::$($response.response.siteUrl)";
 Write-Output "::set-output name=SiteUrl::$($response.response.siteUrl)";
