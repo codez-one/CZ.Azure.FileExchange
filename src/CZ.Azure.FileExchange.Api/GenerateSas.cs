6e756d0b64b94a6949571190d3163c47866ce033
Kirsten Kluge <kirsten.kluge@microsoft.com>
2022-12-15 21:40:02 +0100

    Update Function to dotnet 7

--- src/CZ.Azure.FileExchange.Api/GenerateSas.cs
+++ src/CZ.Azure.FileExchange.Api/GenerateSas.cs
@@ -1,63 +1,71 @@
 namespace CZ.Azure.FileExchange.Api;
 
-using System;
 using System.Net;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
 using global::Azure.Storage.Blobs;
 using global::Azure.Storage.Sas;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Extensions;
-using System.Net.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 public class GenerateSas
 {
     private readonly ILogger<GenerateSas> logger;
 
-    public GenerateSas(ILogger<GenerateSas> log) =>
-        this.logger = log;
+    public GenerateSas(ILoggerFactory loggerFactory)
+    {
+        this.logger = loggerFactory.CreateLogger<GenerateSas>();
+    }
 
-    [FunctionName("GenerateSas")]
-    [OpenApiOperation(operationId: "Run")]
+    [OpenApiOperation(operationId: "greeting", tags: new[] { "sas" }, Summary = "Generate sas token", Description = "This generates a new sas token.", Visibility = OpenApiVisibilityType.Important)]
     [OpenApiParameter(name: "filecode", In = ParameterLocation.Query, Required = false, Type = typeof(string), Description = "The **code** parameter, that represent to get read access to stored files")]
     [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
-    public async Task<IActionResult> Run(
-        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req)
+    [Function("GenerateSas")]
+    public async Task<HttpResponseData> Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequestData req,
+            FunctionContext executionContext,
+            string filecode)
     {
         this.logger.LogInformation("Start generating SaS");
 
         var blobservice = new BlobServiceClient(GetEnvironmentVariable("StorageConnectionString"));
         Uri? uri;
         BlobContainerClient? blobContainerClient;
-        if (req.Query.TryGetValue("filecode", out var code))
+
+        if (!string.IsNullOrWhiteSpace(filecode))
         {
-            blobContainerClient = blobservice.GetBlobContainerClient(code);
+            blobContainerClient = blobservice.GetBlobContainerClient(filecode);
             uri = this.GetServiceSasUriForContainer(blobContainerClient, BlobSasPermissions.Read | BlobSasPermissions.List);
         }
         else
         {
-            var response = await blobservice.CreateBlobContainerAsync(Guid.NewGuid().ToString());
-            _ = response.ThrowIfNullOrDefault();
-            blobContainerClient = response.Value;
+            var blobResponse = await blobservice.CreateBlobContainerAsync(Guid.NewGuid().ToString());
+            _ = blobResponse.ThrowIfNullOrDefault();
+            blobContainerClient = blobResponse.Value;
             uri = this.GetServiceSasUriForContainer(blobContainerClient);
         }
         if (uri == null)
         {
             this.logger.LogError("Failed to generate the Sas token");
-            return new BadRequestObjectResult(new StringContent("Failed to greate SaS token to upload your files. Please try again."));
+            var responseBadRequest = req.CreateResponse(HttpStatusCode.BadRequest);
+            responseBadRequest.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+            responseBadRequest.WriteString("Failed to greate SaS token to upload your files. Please try again.");
+            return responseBadRequest;
         }
 
-        return new OkObjectResult(uri.ToString());
+        var responseOK = req.CreateResponse(HttpStatusCode.OK);
+        responseOK.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+        responseOK.WriteString(uri.ToString());
+        return responseOK;
     }
 
     private static string GetEnvironmentVariable(string name) =>
         Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Process) ??
         throw new ArgumentException($"The setting for {name} is missing.");
+
     private Uri? GetServiceSasUriForContainer(
         BlobContainerClient containerClient,
         BlobSasPermissions permission = BlobSasPermissions.Write | BlobSasPermissions.Read | BlobSasPermissions.List,
